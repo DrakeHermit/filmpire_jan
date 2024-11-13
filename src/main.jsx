6bd02a2e8952e61a7e3f56dbadca0e41bef8d003
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-11-13 12:27:11 +0100

    Implemented redux toolkit and started fetching data from the API

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
 const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </ThemeProvider>
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,24 @@
 import useStyles from "../components/styles";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+
+const tmdbApiKey = import.meta.env.REACT_APP_TMDB_KEY;
+
+import { useGetMoviesQuery } from "../services/TMDB";
 
 const HomePage = () => {
   const classes = useStyles();
+
+  const { data } = useGetMoviesQuery();
+
+  console.log(data);
+  console.log(tmdbApiKey);
 
   return <div className={classes.root}>Movies</div>;
 };
